6fff20404c80d29da3c58fa1d45f1e2ae871aaf9
Albert <johan50693@gmail.com>
2018-06-10 11:26:03 -0400

    Commit inicial

---
+++ myapps/mascota/admin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from myapps.mascota.models import Vacuna, Mascota
+
+# Register your models here.
+
+admin.site.register(Vacuna)
+admin.site.register(Mascota)
