3217e67e30bca78e1cf2568491a653e05d80ed28
Albert <johan50693@gmail.com>
2018-06-12 21:28:28 -0400

    Crear usuarios

--- myapps/usuario/views.py
+++ myapps/usuario/views.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
 
+from myapps.usuario.forms import RegistroForm
 # Create your views here.
+
+class RegistroUsuario(CreateView):
+    model=User
+    template_name="usuario/registrar.html"
+    form_class= RegistroForm
+    success_url= reverse_lazy('mascota:mascota_listar')
