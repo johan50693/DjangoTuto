24632364ecf015769ccb420395f16201d11f0d75
Albert <johan50693@gmail.com>
2018-06-15 21:27:02 -0400

    Se restablece contraseña

--- refugio/settings.py
+++ refugio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,21 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
+
+LOGIN_REDIRECT_URL=reverse_lazy('adopcion:solicitud_listar')
+
+
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 25
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
